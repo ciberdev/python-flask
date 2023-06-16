a38e2cd225eec94edc03c11e8d6bd999ee06b63f
ciberdev <eduardo_garcia@cibinetonline.com>
2023-06-16 08:46:14 -0600

    Update from class

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -1,5 +1,6 @@
 import datetime
 from flask import redirect, render_template, session, request
+from flask.helpers import url_for
 from entities.users import User
 from lib.dbconnection import db
 from repositories.usersds import UsersDS
@@ -7,8 +8,20 @@ from repositories.usersds import UsersDS
 def list():
     servicioDB = UsersDS()
     if request.method == 'POST':
-        resultado = servicioDB.obtenerUsuario(request.form['selected_user'])
-        return render_template('datos_usuario.html', usuario=resultado.User)
+        if(request.form['operation']=='NUEVO'):
+            usuario = User(user_name="", password="", first_name="", last_name="", email_address="")
+            session['object_user'] = usuario
+            return redirect(url_for('bpusers.datos_usuario')) 
+            
+        if(request.form['operation']=='MODIFICAR'):
+            resultado = servicioDB.obtenerUsuario(request.form['selected_user'])
+            session['object_user'] = resultado.User
+            if(resultado != None):
+                return redirect(url_for('bpusers.datos_usuario')) 
+            #render_template('datos_usuario.html', usuario=resultado.User)
+
+        if(request.form['operation']=='ELIMINAR'):
+            pass
         
     datos = {"title": "Modulo de Usuarios", "message":"Bienvenido a nuestro sitio"}
     if (session.get('loggedIn', False)):
@@ -18,8 +31,8 @@ def list():
         return render_template('users.html', datos=datos, users=users)
     return render_template('login.html')     
 
-def nuevo_usuario():
-    usuario = User()
+def datos_usuario():
+    usuario = session['object_user']
     if request.method == 'POST':
         form = request.form
         usuario = User(id=None, user_name=form['user_name'], email_address=form['email_address'],
@@ -30,9 +43,6 @@ def nuevo_usuario():
         db.session.commit()
 
     return render_template('datos_usuario.html', usuario=usuario) 
-
-def modificar_usuario():
-    pass
 
 def eliminar_usuario():
     pass
--- routes/bpusers.py
+++ routes/bpusers.py
@@ -1,8 +1,8 @@
 """Blueprint que describe las rutas del controlador de usuarios"""
 from flask import Blueprint
-from controllers.users_controller import list, nuevo_usuario 
+from controllers.users_controller import list, datos_usuario 
 
 bpusers = Blueprint('bpusers', __name__)
 
 bpusers.route('/', methods=['GET', 'POST'])(list)
-bpusers.route('/nuevo', methods=['GET', 'POST'])(nuevo_usuario)
+bpusers.route('/user', methods=['GET', 'POST'])(datos_usuario)
